c125bcd88bc316bb3b9ed4c50da0cf0bca7fe8db
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2017-12-23 23:04:53 +0100

    cover secondary searches with same construct_url

--- tests/testthat/test-construct_url.R
+++ tests/testthat/test-construct_url.R
@@ -7,4 +7,14 @@ test_that("URL constructions work", {
     construct_url("717"),
     "https://bacdive.dsmz.de/api/bacdive/bacdive_id/717/"
   )
+
+  expect_equal(
+    construct_url("AF12345", searchType = "sequence"),
+    "https://bacdive.dsmz.de/api/bacdive/sequence/AF12345/"
+  )
+
+  expect_equal(
+    construct_url("DSM 319", searchType = "culturecollectionno"),
+    "https://bacdive.dsmz.de/api/bacdive/culturecollectionno/DSM%20319/"
+  )
 })
