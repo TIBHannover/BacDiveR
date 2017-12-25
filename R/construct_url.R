a63b6329a58e1c6ff2ce19a19729092fe1947eee
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2017-12-25 19:26:55 +0100

    warn only if searchType was actually changed

--- R/construct_url.R
+++ R/construct_url.R
@@ -35,6 +35,8 @@ sanitise_input <- function(searchTerm, searchType) {
   # [ ] enable different spellings, abbreviations etc. by funneling them to the
   #   searchTypes / endpoint designations
 
+  searchType_ori <- searchType
+
   if (searchType != "bacdive_id" &
       (grepl("^[0-9]+$", searchTerm))) {
     searchType <- "bacdive_id"
@@ -54,13 +56,15 @@ sanitise_input <- function(searchTerm, searchType) {
     searchType <- "culturecollectionno"
   }
 
-  warning(
-    paste0(
-      "searchType looked very much like a ",
-      searchType,
-      ", so I set it to that. If you don't want me to second-guess your inputs, please set force = FALSE"
+  if (searchType != searchType_ori) {
+    warning(
+      paste0(
+        "searchType looked very much like a ",
+        searchType,
+        ", so I set it to that. If you don't want me to second-guess your inputs, please set force = FALSE"
+      )
     )
-  )
+  }
 
   c(searchTerm, searchType)
 }
