9c8acc97498ce6c55559f1832853cad1bf166bc5
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-01-12 19:21:03 +0100

    reduce BacDive load

--- tests/testthat/test-retrieve_data.R
+++ tests/testthat/test-retrieve_data.R
@@ -1,10 +1,7 @@
 context("test-retrieve_data.R")
 
 test_that("downloading a dataset via BacDive ID works", {
-  expect_true(grepl(
-    "taxonomy_name",
-    download(
-      "https://bacdive.dsmz.de/api/bacdive/bacdive_id/717/?format=json"
-    )
-  ))
+  expect_equal(download(
+      "https://bacdive.dsmz.de/api/bacdive/culturecollectionno/DSM%20319/?format=json"),
+      '[{"url": "https://bacdive.dsmz.de/api/bacdive/bacdive_id/717/"}]')
 })
