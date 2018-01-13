4e23fabd1cc5e42b616411bec6733d8315d829a3
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-01-13 19:01:45 +0100

    fix build error "Illegal character detected!"

--- R/sanitise_input.R
+++ R/sanitise_input.R
@@ -7,7 +7,7 @@
 #' @examples sanitise_input("Bacillus subtilis") # "Bacillus/subtilis"
 #'   sanitise_input("B. subtilis") # ERROR
 sanitise_input <- function(searchTerm, searchType) {
-  if (grepl(pattern = "[^[:alnum:] ]", x = searchTerm))
+  if (grepl(pattern = "[^[:alnum:] ]", x = searchTerm, ignore.case = TRUE))
     stop(
       "Illegal character detected! My apologies, but your search can only contain letters, numbers and white-space. Abbreviating genus names (e.g. 'B. subtilis') is not supported. Please spell out your searchTerm ('Bacillus subtilis'), don't use any 'special' characters and try again."
     )
