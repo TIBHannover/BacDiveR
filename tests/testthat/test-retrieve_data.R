c7bbd0f14bf4f09cfd039a56ceb8a73a792b1050
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-01-16 12:47:42 +0100

    prepare reusing download in several tests

--- tests/testthat/test-retrieve_data.R
+++ tests/testthat/test-retrieve_data.R
@@ -6,13 +6,13 @@ test_that("downloading a dataset via BacDive ID works", {
       '[{"url": "https://bacdive.dsmz.de/api/bacdive/bacdive_id/717/"}]')
 })
 
-test_that("downloading a set of BacDive ID works", {
-  B_subtilis_IDs <-
-    rjson::fromJSON(
-      download(
-        "https://bacdive.dsmz.de/api/bacdive/taxon/Bacillus/subtilis/subtilis/?format=json"
-      )
+
+B_subtilis_IDs <-
+  rjson::fromJSON(
+    download(
+      "https://bacdive.dsmz.de/api/bacdive/taxon/Bacillus/subtilis/subtilis/?format=json"
     )
+  )
 
   expect_equal(names(B_subtilis_IDs),
                c("count", "next", "previous", "results"))
