07218606525a522a665952c7ab1a4b8cea2ef001
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-01-16 16:34:21 +0100

    make control flow more tracable

--- R/util-guess_searchType.R
+++ R/util-guess_searchType.R
@@ -8,33 +8,38 @@
 guess_searchType <- function(searchTerm, searchType) {
   searchType_ori <- searchType
 
-  if (searchType != "bacdive_id" &
+  searchType <- dplyr::case_when(
+    searchType != "bacdive_id" &
       grepl("^[0-9]+$", searchTerm) |
       searchType == "bacdive-id" |
       searchType == "bacdiveid" |
-      searchType == "id") {
-    searchType <- "bacdive_id"
-  } else if (searchType != "sequence" &
-             grepl(pattern = "([A-Z][0-9]{5}|[A-Z]{2}[0-9]{6})",
-                   x = searchTerm) |
-             searchType == "accession") {
+      searchType == "id"
+    ~ "bacdive_id",
+
     # detect nucleotide accession / sequence numbers by matching:
     # 1 letter + 5 numerals OR 2 letters + 6 numerals
     # https://www.ncbi.nlm.nih.gov/Sequin/acc.html
-    searchType <- "sequence"
-  }
-  else if (searchType != "culturecollectionno" &
-           grepl(x = searchTerm,
-                 pattern = "(ACM|ATCC|CIP|DSM|ICPB|JCM|LMG|NCDO|NCTC|PS|QUM)+ [0-9]+(-[0-9]+)?") |
-           searchType == "culturenumber" |
-           searchType == "cultureno" |
-           searchType == "collection" |
-           searchType == "ccn") {
-    searchType <- "culturecollectionno"
-  } else if (searchType != "taxon" &
-             !grepl("[0-9]+", x = searchTerm)) {
-    searchType <- "taxon"
-  }
+    searchType != "sequence" &
+      grepl(pattern = "([A-Z][0-9]{5}|[A-Z]{2}[0-9]{6})",
+            x = searchTerm) |
+      searchType == "accession"
+    ~ "sequence",
+
+    searchType != "culturecollectionno" &
+      grepl(x = searchTerm,
+            pattern = "(ACM|ATCC|CIP|DSM|ICPB|JCM|LMG|NCDO|NCTC|PS|QUM)+ [0-9]+(-[0-9]+)?") |
+      searchType == "culturenumber" |
+      searchType == "cultureno" |
+      searchType == "collection" |
+      searchType == "ccn"
+    ~ "culturecollectionno",
+
+    searchType != "taxon" &
+      !grepl("[0-9]+", x = searchTerm)
+    ~ "taxon",
+
+    TRUE ~ searchType
+  )
 
   if (searchType != searchType_ori) {
     warning(
