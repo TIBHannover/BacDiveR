595474ac266e4d5d28ed7151407ffe02d00a7e65
Katrin Leinweber <kalei@posteo.de>
2018-04-04 20:12:00 +0200

    Rename return variable more logically

--- R/retrieve_data.R
+++ R/retrieve_data.R
@@ -57,14 +57,14 @@ aggregate_datasets <- function(payload)
   IDs <- URLs_to_IDs(URLs)
   message("Data download in progress for BacDive-IDs: ", appendLF = FALSE)
 
-  taxon_data <- list()
+  datasets <- list()
   for (i in seq(length(URLs))) {
     message(IDs[i], " ", appendLF = FALSE)
-    taxon_data[i] <- download(paste0(URLs[i], "?format=json"))
+    datasets[i] <- download(paste0(URLs[i], "?format=json"))
   }
-  taxon_data <- lapply(taxon_data, jsonlite::fromJSON)
-  names(taxon_data) <- IDs
-  return(taxon_data)
+  datasets <- lapply(datasets, jsonlite::fromJSON)
+  names(datasets) <- IDs
+  return(datasets)
 }
 
 
