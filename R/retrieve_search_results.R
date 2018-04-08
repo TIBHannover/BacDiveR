eddf203a0feface885494fe44709b2635072a7bf
Katrin Leinweber <kalei@posteo.de>
2018-04-08 11:54:44 +0200

    Document donwload of advanced search results

--- R/retrieve_search_results.R
+++ R/retrieve_search_results.R
@@ -1,3 +1,15 @@
+#' Retrieve Results of an Advanced Search/Query
+#'
+#' @param queryURL URL of a query prepated on [BacDive.DSMZ.de/AdvSearch
+#'   ](https://bacdive.dsmz.de/advsearch), as explained in the vignette "The
+#'   semi-automatic approach: retrieving data through a pre-configured advanced
+#'   search".
+#'
+#' @inherit retrieve_data return
+#' @export
+#'
+#' @examples retrieve_search_results(queryURL = "https://bacdive.dsmz.de/advsearch?site=advsearch&searchparams[78][contenttype]=text&searchparams[78][typecontent]=contains&searchparams[78][searchterm]=Miller&advsearch=search")
+#'
 retrieve_search_results <- function(queryURL) {
 
   download_param <- "&csv_bacdive_ids_advsearch=download"
