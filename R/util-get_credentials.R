5ef5400502e4a799c97e9f42989070d2c77c0ce2
Katrin Leinweber <kalei@posteo.de>
2018-04-23 15:04:37 +0200

    Remove obsolete code for old tests

--- R/util-get_credentials.R
+++ R/util-get_credentials.R
@@ -1,18 +1,11 @@
 #' Read BacDive Login Credentials from .Renviron
 #'
-#' @param test Logical (default: `FALSE`). Only used by automatic tests.
-#'
 #' @return A vector or the two character strings `BacDive_email` and `BacDive_password` representing those two environment variables.
-get_credentials <- function(test = FALSE) {
+get_credentials <- function() {
   readRenviron(get_Renviron_path())
 
-  if (test == TRUE) {
-    id <- Sys.getenv("BacDive_test_email")
-    pw <- Sys.getenv("BacDive_test_password")
-  } else {
-    id <- Sys.getenv("BacDive_email")
-    pw <- Sys.getenv("BacDive_password")
-  }
+  id <- Sys.getenv("BacDive_email")
+  pw <- Sys.getenv("BacDive_password")
 
   if (!nzchar(id) | !nzchar(pw))
     warning("BacDive login credentials not set completely.\n
