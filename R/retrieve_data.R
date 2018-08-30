ee9daf8311d7baa97bfd82bdf3405e27b7c850cb
Katrin Leinweber <kalei@posteo.de>
2018-08-30 21:57:02 +0200

    Move download function to own file

--- R/retrieve_data.R
+++ R/retrieve_data.R
@@ -46,32 +46,6 @@ retrieve_data <- function(searchTerm,
 }
 
 
-#' Download Something from BacDive
-#'
-#' @param URL represented by a correctly encoded character string with spaces,
-#'   thanks to utils::URLencode.
-#' @param userpwd A character string of the format
-#'   `BacDive_email:BacDive_password`. Retrieved from `.Renviron` my default,
-#'   but also used with something else by the tests.
-#'
-#' @return A serialised JSON string.
-download <-
-  function(URL,
-           userpwd = paste(get_credentials(), collapse = ":"))
-  {
-    message(URLs_to_IDs(URL), " ", appendLF = FALSE)
-
-    payload <- RCurl::getURL(URL,
-                             userpwd = userpwd,
-                             httpauth = 1L)
-
-    repair_escaping(payload)
-    # Needs to remain here, not between jsonlite::fromJSON & download above,
-    # because retrieve_search_results() doesn't call it directly, but only
-    # through aggregate_datasets().
-  }
-
-
 is_dataset <- function(payload)
 {
   identical(
