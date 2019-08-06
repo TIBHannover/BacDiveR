e148c9c741d233249c026988850c6673f117ce40
Katrin Leinweber <Katrin.Leinweber@tib.eu>
2019-08-06 18:18:48 +0200

    Refactor downloader to accept credentials input

--- R/util-download.R
+++ R/util-download.R
@@ -1,16 +1,16 @@
 #' Download Something from BacDive
 #'
+#' Both `user` and `password` are read from the .Renviron file by default.
+#'
 #' @param URL A correctly encoded character string, either from construct_url()
 #'   or a JSON list
 #'
 #' @return The R object deserialised from the downloaded JSON
 #' @keywords internal
-download <- function(URL) {
+download <- function(URL, user = get_credentials()[1], password = get_credentials()[2]) {
   message(URLs_to_IDs(URL), " ", appendLF = FALSE)
 
-  cred <- get_credentials()
-
-  response <- httr::GET(URL, httr::authenticate(cred[1], cred[2]))
+  response <- httr::GET(URL, httr::authenticate(user, password))
   payload <- httr::content(response, as = "text", encoding = "UTF-8")
   jsonlite::fromJSON(payload)
 }
