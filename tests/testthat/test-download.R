e38a2f5331152f0961e9c0b17b4decbd986258b0
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2020-06-29 09:58:40 +0200

    Fix typo

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -1,4 +1,4 @@
-test_that("Downloading without preper credentials raises an error", {
+test_that("Downloading without proper credentials raises an error", {
   random <- sample(letters, 8)
   expect_error(download(construct_url(717), user = random, password = random))
 })
